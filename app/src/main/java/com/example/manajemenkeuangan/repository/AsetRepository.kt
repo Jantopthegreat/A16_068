37ec723a9ca07187c0a2437bdacdf39def045220
Jantopthegreat <fauzangriya@gmail.com>
2025-01-25 18:04:39 +0700

    Pembuatan Aset NetworkRepository

--- app/src/main/java/com/example/manajemenkeuangan/repository/AsetRepository.kt
+++ app/src/main/java/com/example/manajemenkeuangan/repository/AsetRepository.kt
@@ -17,3 +17,38 @@ interface AsetRepository {
 
     suspend fun getAsetById(idAset: Int): AsetDetailResponse
 }
+
+class NetworkAsetRepository(
+    private val asetApiService: AsetService
+) : AsetRepository {
+
+    override suspend fun insertAset(aset: Aset) {
+        asetApiService.insertAset(aset)
+    }
+
+    override suspend fun updateAset(idAset: Int, aset: Aset) {
+        asetApiService.updateAset(idAset, aset)
+    }
+
+    override suspend fun deleteAset(idAset: Int) {
+        try {
+            val response = asetApiService.deleteAset(idAset)
+            if (!response.isSuccessful) {
+                throw Exception("Gagal menghapus aset, HTTP Status Code: " + "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getAllAset(): AsetResponse {
+        return asetApiService.getAllAset()
+    }
+
+    override suspend fun getAsetById(idAset: Int): AsetDetailResponse {
+        return asetApiService.getAsetById(idAset)
+    }
+}
