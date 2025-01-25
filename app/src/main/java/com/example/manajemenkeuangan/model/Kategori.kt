c07aabff108d8d03cf46dda30d8b0a211c1ac082
Jantopthegreat <fauzangriya@gmail.com>
2025-01-25 17:39:32 +0700

    Pembuatan Kategori detail response dan kategori response

--- app/src/main/java/com/example/manajemenkeuangan/model/Kategori.kt
+++ app/src/main/java/com/example/manajemenkeuangan/model/Kategori.kt
@@ -3,7 +3,17 @@ package com.example.manajemenkeuangan.model
 import kotlinx.serialization.Serializable
 
 
+@Serializable
+data class KategoriResponse(
+    val status : Boolean,
+    val data: List<Kategori>
+)
 
+@Serializable
+data class KategoriDetailResponse(
+    val status: Boolean,
+    val data: Kategori
+)
 
 @Serializable
 data class Kategori (
